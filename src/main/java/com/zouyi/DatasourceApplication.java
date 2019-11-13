69653c585875cf6e8aef8217b1c81254d7f28d10
dimo <676642583@qq.com>
2019-11-13 10:49:39 +0800

    时间紧，上个版本没有测试，稍作修改可以正常用了

--- src/main/java/com/zouyi/DatasourceApplication.java
+++ src/main/java/com/zouyi/DatasourceApplication.java
@@ -1,11 +1,18 @@
 package com.zouyi;
 
+import com.zouyi.test.config.DynamicDataSourceConfig;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+@MapperScan(basePackages = "com.zouyi.test.mapper")
+@Import({DynamicDataSourceConfig.class})
 public class DatasourceApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(DatasourceApplication.class, args);
     }
--- src/main/java/com/zouyi/test/controller/TestController.java
+++ src/main/java/com/zouyi/test/controller/TestController.java
@@ -6,7 +6,6 @@ import com.zouyi.test.dto.OrderDTO;
 import com.zouyi.test.dto.UserOrderDTO;
 import com.zouyi.test.dto.UserTDO;
 import com.zouyi.test.service.TestService;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -25,23 +24,19 @@ public class TestController {
     @Autowired
     private TestService testService;
 
-    @ApiOperation(value = "测试获取数据库1数据")
-    @GetMapping("/test1")
+    @GetMapping("/test1")//测试获取数据库1数据
     public APIResult<List<UserTDO>> test1() {
         List<UserTDO> list = testService.test1();
         return APIResultUtil.success(list);
     }
 
-    @ApiOperation(value = "测试获取数据库2数据")
-    @GetMapping("/test2")
+    @GetMapping("/test2")//测试获取数据库2数据
     public APIResult<List<OrderDTO>> test2() {
         List<OrderDTO> list = testService.test2();
         return APIResultUtil.success(list);
     }
 
-
-    @ApiOperation(value = "同时获取两个数据库数据")
-    @GetMapping("/test3/{userId}")
+    @GetMapping("/test3/{userId}")//同时获取两个数据库数据
     public APIResult<UserOrderDTO> test3(@PathVariable(value = "userId") Integer userId) {
         UserOrderDTO userOrderDTO = testService.test3(userId);
         return APIResultUtil.success(userOrderDTO);
